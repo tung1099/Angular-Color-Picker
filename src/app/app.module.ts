eebf5de6c6c66db6bb849895d27846dc50eb96a4
tung1099 <dtroyal1102@gmail.com>
2022-05-24 11:28:06 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ColorComponent } from './color/color.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ColorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
